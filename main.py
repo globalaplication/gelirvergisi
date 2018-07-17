a76e4f0b7e2c66ae932bfd5c6f9526a2c2225fdf
Nuri Özdaş <32783197+globalaplication@users.noreply.github.com>
2018-07-18 02:54:07 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 cizelge = {
     150000:25, 300000:30, 
     600000:35, 1200000:40
@@ -12,6 +11,7 @@ def vergial(geliri, yuzde=0):
     if yuzde is 0:
         yuzde = 50
     return "vergi yuzdesi %{}, {} Tl vergi alinmali ".format(yuzde, 
-    geliri * yuzde /100)
+    str(geliri * yuzde /100)
+    [0:-2])
 
-print (vergial(600000))
+print (vergial(599000))
