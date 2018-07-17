2d7d78339a62d14fd308c312cd90e00c6bd253b8
Nuri Özdaş <32783197+globalaplication@users.noreply.github.com>
2018-07-18 00:34:06 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-
 cizelge = {150.00:25, 
 300.00:30, 
 600.00:35, 900.00:45}
-
-def vergial(geliri, yuzde=0):
+def vergial(geliri, yuzde = 0):
     for test in cizelge:
         if  test <= geliri:
             yuzde = cizelge[test]
-    return geliri * yuzde /100
+    return geliri * yuzde / 100
+
 print ("gelirinden {} Tl vergi alinmali ".format( vergial(200) ))
