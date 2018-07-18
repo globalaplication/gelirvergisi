49e7af09289f8ccaa9d46b1330586dff4dc83e4e
Nuri Özdaş <32783197+globalaplication@users.noreply.github.com>
2018-07-18 03:27:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 cizelge = {
     150000:25, 300000:30, 
     600000:35, 1200000:40
@@ -13,7 +12,7 @@ def ondalik(tl, test=""):
         return test[::-1][1:]
     return test[::-1]
 def vergial(geliri, yuzde=0):
-    geliri = geliri.replace(".","")
+    geliri = geliri.replace(".","").replace(",","")
     geliri = float(geliri)
     for test in cizelge:
         if geliri <= test :
@@ -21,11 +20,11 @@ def vergial(geliri, yuzde=0):
             break
     if yuzde is 0:
         yuzde = 50
-    return "vergi yuzdesi %{}, {} Tl vergi alinmali ".format(yuzde, 
+    return "vergi yuzdesi %{}, {} Tl vergi alinacak.".format(yuzde,
     ondalik(str(geliri * yuzde /100)
     [0:-2]))
 
-print (vergial("1.200.000"))
+print (vergial("1.200.500"))
 
 
 
